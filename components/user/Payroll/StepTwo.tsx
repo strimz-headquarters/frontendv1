e70efcf636a11910d05e58e00fa55785b19c1e9c
signor1 <emmanuelomemgboji@gmail.com>
2025-01-11 17:07:01 +0100

    feat: validated CSV data

--- components/user/Payroll/StepTwo.tsx
+++ components/user/Payroll/StepTwo.tsx
@@ -1,3 +1,6 @@
+'use client'
+import { useState } from "react";
+import { formatFileSize, useCSVReader } from "react-papaparse";
 import {
     Dialog,
     DialogContent,
@@ -6,8 +9,52 @@ import {
     DialogTrigger,
 } from "@/components/ui/dialog"
 import { LuArrowDownToLine } from "react-icons/lu"
+import Link from "next/link";
+
+type CVSDataType = {
+    name: string;
+    address: `0x${string}`;
+    amount: number;
+}
 
 const StepTwoForm = ({ handleClick }: { handleClick: () => void }) => {
+    const [csvData, setCsvData] = useState<CVSDataType[]>([]);
+    const [error, setError] = useState<string | null>(null);
+
+    const { CSVReader } = useCSVReader();
+    const [zoneHover, setZoneHover] = useState(false);
+
+    const validateCSVData = (data: any[]): boolean => {
+        if (!data || data.length === 0) {
+            setError("The uploaded file is empty. Please check the file.");
+            return false;
+        }
+
+        for (const row of data) {
+            // Check if name exists and is a string
+            if (typeof row.name !== "string" || row.name.trim() === "") {
+                setError("Invalid data: 'name' field must be a non-empty string.");
+                return false;
+            }
+
+            // Check if amount exists and is a valid number
+            const amount = Number(row.amount);
+            if (isNaN(amount) || amount <= 0) {
+                setError("Invalid data: 'amount' must be a positive number.");
+                return false;
+            }
+
+            // Optional: Add address validation if required
+            if (row.address && !row.address.startsWith("0x")) {
+                setError("Invalid data: 'address' must start with '0x'.");
+                return false;
+            }
+        }
+
+        setError(null);
+        return true;
+    };
+
     return (
         <main className="w-full flex flex-col gap-5">
             <div className="w-full flex flex-col">
@@ -19,7 +66,7 @@ const StepTwoForm = ({ handleClick }: { handleClick: () => void }) => {
             {/* table section */}
             <div className="w-full flex flex-col gap-4">
                 {/* csv import modal */}
-                <div className="w-full justify-end">
+                <div className="w-full flex justify-end">
                     <Dialog>
                         <DialogTrigger asChild>
                             <button type="button" className='w-[120px] h-[32px] flex justify-center items-center gap-2 bg-[#F9FAFB] rounded-[8px] border border-[#E5E7EB] shadow-[0px_-2px_4px_0px_#00000014_inset] cursor-pointer text-[12px] font-[500] font-poppins text-strimzPrimary capitalize'>
@@ -36,7 +83,81 @@ const StepTwoForm = ({ handleClick }: { handleClick: () => void }) => {
                                     <span className="text-black font-[500] font-sora capitalize text-sm ">Import .CSV file</span>
                                 </DialogTitle>
                             </DialogHeader>
-                            <div className=""></div>
+                            <div className="w-full">
+                                {/* Download Sample CSV */}
+                                <Link href="/sample.csv" download className="text-xs font-poppins font-[400] text-[#58556A] underline">
+                                    Download Sample CSV
+                                </Link>
+                                <CSVReader
+                                    config={{ header: true, skipEmptyLines: true }}
+                                    onUploadAccepted={(results: any) => {
+                                        const data = results.data;
+                                        if (validateCSVData(data)) {
+                                            setCsvData(data);
+                                            setError(null);
+                                        } else {
+                                            setCsvData([]);
+                                        }
+                                        setZoneHover(false);
+                                    }}
+                                    onDragOver={(event: DragEvent) => {
+                                        event.preventDefault();
+                                        setZoneHover(true);
+                                    }}
+                                    onDragLeave={(event: DragEvent) => {
+                                        event.preventDefault();
+                                        setZoneHover(false);
+                                    }}
+                                >
+                                    {({
+                                        getRootProps,
+                                        acceptedFile,
+                                        ProgressBar,
+                                        getRemoveFileProps,
+                                    }: any) => (
+                                        <div
+                                            {...getRootProps()}
+                                            className={`flex h-[200px] mt-2 flex-col items-center justify-center border-2 border-dashed rounded-lg p-6 transition-all duration-300 ${zoneHover ? "border-strimzBrandAccent bg-[#F9FAFB]" : "border-[#E5E7EB] bg-[#F9FAFB]"
+                                                }`}
+                                        >
+                                            {acceptedFile ? (
+                                                <>
+                                                    <div className="flex flex-col items-center w-full">
+                                                        <div className="flex flex-col items-center">
+                                                            <span className="text-strimzPrimary font-poppins font-[400] text-sm">
+                                                                {formatFileSize(acceptedFile.size)}
+                                                            </span>
+                                                            <span className="text-strimzPrimary text-sm font-poppins font-medium">
+                                                                {acceptedFile.name}
+                                                            </span>
+                                                        </div>
+                                                        <div className="w-full mt-2">
+                                                            <ProgressBar />
+                                                        </div>
+                                                        <button
+                                                            {...getRemoveFileProps()}
+                                                            className="mt-2 px-3 py-1 text-xs font-medium text-white bg-rose-500 rounded hover:bg-rose-600"
+                                                        >
+                                                            Remove File
+                                                        </button>
+                                                    </div>
+                                                </>
+                                            ) : (
+                                                <div className="flex flex-col gap-1.5 items-center cursor-pointer ">
+                                                    <LuArrowDownToLine className="w-6 h-6 text-[#58556A]" />
+                                                    <h3 className="capitalize text-base font-sora cursor-pointer font-[500] text-[#58556A]">
+                                                        Import CSV File
+                                                    </h3>
+
+                                                </div>
+                                            )}
+                                        </div>
+                                    )}
+                                </CSVReader>
+                                {error && (
+                                    <p className="text-red-600 text-sm mt-3">{error}</p>
+                                )}
+                            </div>
                         </DialogContent>
                     </Dialog>
                 </div>
