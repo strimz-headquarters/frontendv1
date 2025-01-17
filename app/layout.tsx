75610e8de7b1d10dc33c25b15afb9ea7bcb38e6d
signor1 <emmanuelomemgboji@gmail.com>
2025-01-17 16:24:42 +0100

    feat: updated manual SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 import { getMetadata } from "@/utils/getMetadata";
 import { Toaster } from 'sonner';
 
@@ -14,6 +15,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              "url": "https://www.strimz.xyz",
+              "name": "Strimz",
+              "description": "Strimz automates crypto payroll and subscription payments, offering secure and seamless financial management.",
+            }),
+          }}
+        />
+      </Head>
       <body
         className="w-full min-h-screen antialiased bg-[#FFFFFF]"
       >
---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.strimz.xyz",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
